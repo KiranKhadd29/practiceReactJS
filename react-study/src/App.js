839b1937c09c1cf99842f76662e04299f35cd3f5
Kiran Khadd <kirank@prorigo.com>
2024-01-27 07:40:22 +0530

    TextUtils - States and Handling events

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,12 +1,15 @@
 //import logo from './logo.svg';
 //import './App.css';
-
 import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
 
 function App() {
   return (
     <>
     <Navbar title="testUtils" />
+    <div className="container my-3">
+      <TextForm heading = "Enter text below" />
+    </div>
     </>
   );
 }
