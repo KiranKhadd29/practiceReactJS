50f62e961423a938460a44169ee897a0d291cdd9
Kiran Khadd <kirank@prorigo.com>
2024-02-11 21:29:53 +0530

    textUtils : About Us with Dark Mode & some new buttoons in TextForm

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -2,12 +2,14 @@
 //import './App.css';
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   return (
     <>
-    <Navbar title="testUtils" />
+    <Navbar title="TextUtils" />
     <div className="container my-3">
+      {/*<AboutUs/>*/}
       <TextForm heading = "Enter text below" />
     </div>
     </>
