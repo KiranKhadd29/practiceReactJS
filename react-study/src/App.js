06668fc9b18f635363702b6a0698f3ed07b2ed77
Kiran Khadd <kirank@prorigo.com>
2024-02-12 19:12:33 +0530

    testUtils : Dark mode for whole page

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -2,18 +2,34 @@
 //import './App.css';
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-import AboutUs from "./Components/AboutUs";
+//import AboutUs from "./Components/AboutUs";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState ('dark');
+  const toggleMode =() => {
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor=('#1a3358');
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor=('white');
+    }
+
+  }
   return (
     <>
-    <Navbar title="TextUtils" />
+    <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode} />
     <div className="container my-3">
       {/*<AboutUs/>*/}
-      <TextForm heading = "Enter text below" />
+      <TextForm heading = "Enter text below" mode ={mode} />
     </div>
     </>
   );
 }
+
 
 export default App;
--- react-study/src/Components/TextForm.js
+++ react-study/src/Components/TextForm.js
@@ -53,28 +53,28 @@ export default function TextForm(props) {
     //setText = {"new text"} // correct way to change the state
   return (
     <>
-    <div className='container'>
-      <h1>{props.heading}</h1>
+    <div className='container' style= {{color : props.mode === 'dark'?'white':'#1a3358'}}>
+      <h1 style= {{color : props.mode === 'dark'?'white':'#1a3358'}}>{props.heading}</h1>
       <div className="mb-3">
           {/*<label for="myBox" className="form-label">Example textarea</label>*/}
-          <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} placeholder="Enter Text Here ..." rows="8"></textarea>
+          <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} style= {{backgraoundColor : props.mode === 'dark'?'grey':'light', color : props.mode === 'dark'?'#1a3358':'#1a3358'}} placeholder="Enter Text Here ..." rows="8"></textarea>
           <p></p>
           <button className="btn btn-secondary mx-1" onClick={handleUndoClick}>Undo </button>
           <button className="btn btn-secondary mx-1" onClick={handleRedoClick}>Redo </button>
-          <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase </button>
-          <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase </button>
+          <button className="btn btn-primary mx-1" onClick={handleUpClick}>UpperCase </button>
+          <button className="btn btn-primary mx-1" onClick={handleLowClick}>LowerCase </button>
           <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text </button>
           <button className="btn btn-primary mx-1" onClick={handleClearTextClick}>Clear Text </button>
           <button className="btn btn-primary mx-1" onClick={handleExtraSpaceClick}>Remove Extra Spaces </button>
 
       </div>
     </div>
-    <div className='container my-3'>
+    <div className='container my-3' style= {{color : props.mode === 'dark'?'white':'#1a3358'}}>
       <h2>Your Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters </p>
       <p>{0.08 * text.split(" ").length} minute to read </p>
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something to preview!"}</p>
 
     </div>
     </>
